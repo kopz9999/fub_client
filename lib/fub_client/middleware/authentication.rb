8770813b7bb64230b824fe4802aaf121cafbc446
Kyoto Kopz <kopz9999@gmail.com>
2016-08-04 15:42:31 -0400

    Optional API Key based on Thread

--- lib/fub_client/middleware/authentication.rb
+++ lib/fub_client/middleware/authentication.rb
@@ -2,8 +2,10 @@ module FubClient
   module Middleware
     class Authentication < Faraday::Middleware
       def call(env)
+        api_key = Thread.current[:fub_api_key] || 
+          FubClient::Client.instance.api_key
         env[:request_headers]["Authorization"] = "Basic " + 
-          Base64.strict_encode64("#{FubClient::Client.instance.api_key}:")
+          Base64.strict_encode64("#{api_key}:")
         @app.call(env)
       end
     end
