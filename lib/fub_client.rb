86310a584523995358ab372d48a88b1f31270c1e
Linch <andrei95-1995@mail.ru>
2017-06-13 18:01:53 +0300

    added Deals

--- lib/fub_client.rb
+++ lib/fub_client.rb
@@ -21,6 +21,7 @@ require "fub_client/email_template"
 require "fub_client/action_plan"
 require "fub_client/em_event"
 require "fub_client/custom_field"
+require "fub_client/deal"
 
 module FubClient
   def self.root
